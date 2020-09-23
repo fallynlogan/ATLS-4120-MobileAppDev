cf24e3ab274613c3880da70c33da12391ecf0a38
Fallyn Logan <49659834+fallynlogan@users.noreply.github.com>
2020-09-22 21:12:10 -0600

    Add files via upload

---
+++ lab_3/lab_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab_3
+//
+//  Created by Fallyn Logan on 9/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
